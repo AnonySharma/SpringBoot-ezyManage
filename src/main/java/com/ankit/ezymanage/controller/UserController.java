f41db485f0f6cd16833ea2539a36c347ecc9224b
Ankit Sharma <ankit.kumar.cse19@itbhu.ac.in>
2021-11-13 01:47:03 +0530

    Minor Fix

--- src/main/java/com/ankit/ezymanage/controller/UserController.java
+++ src/main/java/com/ankit/ezymanage/controller/UserController.java
@@ -92,4 +92,51 @@ public class UserController extends BaseController {
 		System.out.println(model.toString());
 		return "redirect:/profile/";
 	}
+
+	// My orders
+	@GetMapping("/myorders/")
+	public String myOrders(Model model) {
+		isLoggedIn();
+		System.out.println("Welcome to My Orders!");
+		isAuthorized(model, "ROLE_USER");
+		List<Order> orders = orderService.getAllOrdersByUserId(user.getId());
+		List<Pair<Order, String>> myOrderList = new ArrayList<>();
+		for (Order order : orders) {
+			String products = "";
+			for (Pair<Integer, Integer> product : order.getItems()) {
+				products += productService.getProduct(product.getFirst()).getName() + "(x" + product.getSecond()
+						+ "), ";
+			}
+			products = products.substring(0, products.length() - 2);
+			myOrderList.add(new Pair<>(order, products));
+		}
+		Map<Integer, String> shopMap = new HashMap<>();
+		for (Pair<Order, String> order : myOrderList) {
+			shopMap.put(order.getFirst().getShopId(), shopService.getShopById(order.getFirst().getShopId()).getName());
+		}
+		model.addAttribute("shopMap", shopMap);
+		model.addAttribute("orders", myOrderList);
+		return "my_orders";
+	}
+
+	@GetMapping("/myorders/{orderId}/")
+	public String pastOrderItem(@PathVariable("orderId") int orderId, Model model) {
+		Order order = orderService.getOrderByOrderId(orderId);
+		model.addAttribute("orderId", orderId);
+		model.addAttribute("shopName",
+				shopService.getShopById(orderService.getOrderByOrderId(orderId).getShopId()).getName());
+		model.addAttribute("customerName", userService.getUserById(order.getCustomerId()).getUsername());
+		model.addAttribute("staffName", userService.getUserById(order.getStaffId()).getUsername());
+		model.addAttribute("order", order);
+
+		List<Pair<Product, Integer>> orderedItems = new ArrayList<>();
+
+		for (Pair<Integer, Integer> product : order.getItems()) {
+			orderedItems.add(new Pair<Product, Integer>(productService.getProduct(product.getFirst().intValue()),
+					product.getSecond()));
+		}
+		model.addAttribute("orderedItems", orderedItems);
+		model.addAttribute("backLink", "/myorders/");
+		return "order_page";
+	}
 }
