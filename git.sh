304775f8d6fb5e4f0bc3bf893ae5722d2f25873e
Ankit Sharma <ankit.kumar.cse19@itbhu.ac.in>
2021-11-13 05:44:22 +0530

    Added Deployment and Git Shell file

---
+++ git.sh
@@ -0,0 +1,6 @@
+read -p "Commit Message: " msg
+cp src/main/resources/application-remote.properties src/main/resources/application.properties
+git stage --all
+git commit -m "$msg"
+git push origin master
+cp src/main/resources/application-local.properties src/main/resources/application.properties
