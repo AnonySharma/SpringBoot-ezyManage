7a67dace4d2dd95d0d879fec450a72f275d20373
Ankit Sharma <ankit.kumar.cse19@itbhu.ac.in>
2021-11-27 12:16:57 +0530

    Fixing email verification

--- src/main/java/com/ankit/ezymanage/utils/GetHost.java
+++ src/main/java/com/ankit/ezymanage/utils/GetHost.java
@@ -5,9 +5,11 @@ import java.net.UnknownHostException;
 public class GetHost {
     public static String getHost() {
         String host = "";
+        Boolean isLocal = false;
         try {
             host = java.net.InetAddress.getLocalHost().getHostName();
-            System.out.println(host);
+            isLocal = java.net.InetAddress.getLocalHost().isAnyLocalAddress();
+            System.out.println(host + " " + isLocal);
         } catch (UnknownHostException e) {
             e.printStackTrace();
             System.out.println(e);
