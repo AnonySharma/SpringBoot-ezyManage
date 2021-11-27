daf14faa23f6040cc2a68fafa74d242361f13c3e
Ankit Sharma <ankit.kumar.cse19@itbhu.ac.in>
2021-11-27 12:19:47 +0530

    Fixing email verification

--- src/main/java/com/ankit/ezymanage/utils/GetHost.java
+++ src/main/java/com/ankit/ezymanage/utils/GetHost.java
@@ -9,7 +9,10 @@ public class GetHost {
         try {
             host = java.net.InetAddress.getLocalHost().getHostName();
             isLocal = java.net.InetAddress.getLocalHost().isAnyLocalAddress();
-            System.out.println(host + " " + isLocal);
+            System.out.println(java.net.InetAddress.getLocalHost().getHostAddress());
+            System.out.println(java.net.InetAddress.getLocalHost().getHostName());
+            System.out.println(java.net.InetAddress.getLocalHost().isAnyLocalAddress());
+            System.out.println(java.net.InetAddress.getLocalHost().getCanonicalHostName());
         } catch (UnknownHostException e) {
             e.printStackTrace();
             System.out.println(e);
